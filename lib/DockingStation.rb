1c545a94b2b85768f9ab204d1202643d356cf852
Christopher Weeks <christopherweeks@Christophers-MacBook-Pro.local>
2015-09-02 17:59:08 +0100

    post-challenge 1

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,5 +1,18 @@
 class DockingStation
+  def initialize
+    @bikes = 0
+  end
+
   def release_bike
-    Bike.new
+      if @bikes < 1
+        fail 'No bikes available'
+      else
+        @bikes -= 1
+        Bike.new
+      end
+  end
+
+  def dock(bike)
+    @bikes =+ 1
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,11 +2,28 @@ require 'DockingStation'
 require 'Bike'
 
 describe DockingStation do
-    dock = DockingStation.new
-    bike = dock.release_bike
-    it { is_expected.to respond_to :release_bike }
-    it { expect(bike).to be_a Bike }
-    it 'bike is working' do
-      expect(bike.working?).to be true
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+  
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to :release_bike }
+
+  it 'Creates bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    bike = subject.release_bike
+    expect(bike).to be_a Bike
+  end
+
+  it 'bike is working' do
+    bike = Bike.new
+    subject.dock(bike)
+    bike = subject.release_bike
+    expect(bike.working?).to be true
     end
 end
